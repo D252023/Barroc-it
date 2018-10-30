47e8c7ebc2ac5e108c96d35c0af63fd9a65a35c6
Unknown <D252023@edu.rocwb.nl>
2018-10-30 15:21:46 +0100

    added auth system

--- barroc_it/database/migrations/2014_10_12_000000_create_users_table.php
+++ barroc_it/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('DepartmentID')->nullable();
+            $table->string('type')->default('default');
             $table->rememberToken();
             $table->timestamps();
         });
